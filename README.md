dec87bfa19225e13af360f935099a103246f0bb1
Kaique <kaiqueboscoaraujo00@gmail.com>
2025-10-30 09:48:11 -0300

    .

--- README.md
+++
@@ -1,1 +0,0 @@
-# exercicios-funcoes-nvMedio-py
---
+++ etapa_1/func_operacoes.py
@@ -0,0 +1,6 @@
+# Crie uma função operacoes(a, b) que retorne a soma, subtração, multiplicação e divisão de a e b.
+import os
+os.system('clear')
+
+def operacoes(a, b):
+    
