8fa8a04ca5c40e8960ce8ca0a52a114bf5383347
Kaique <kaiqueboscoaraujo00@gmail.com>
2025-10-30 09:54:04 -0300

    .

--- etapa_1/func_operacoes.py
+++ etapa_1/func_operacoes.py
@@ -3,4 +3,19 @@ import os
 os.system('clear')
 
 def operacoes(a, b):
+
+    soma = a + b
+    sub = a - b
+    mult = a * b
+    div = a / b
+    resultados = [soma, sub, mult, div]
+
+    print(f'Soma: {a} + {b} = {soma}\n'
+          f'Subtração: {a} - {b} = {sub}\n'
+          f'Multiplicação: {a} x {b} = {mult}\n'
+          f'Divisão: {a} / {b} = {div}\n')
     
+    return resultados
+    
+
+print(operacoes(5, 5))
