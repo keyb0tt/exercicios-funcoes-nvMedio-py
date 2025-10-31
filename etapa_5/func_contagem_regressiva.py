5378675594f70e5366d1d783c58ac33c8aedffb5
Kaique <kaiqueboscoaraujo00@gmail.com>
2025-10-31 09:34:37 -0300

    .

--- etapa_5/func_contagem_regressiva.py
+++ etapa_5/func_contagem_regressiva.py
@@ -2,3 +2,13 @@
 import os
 os.system('clear')
 
+def contagem_regressiva(n):
+    # Caso base
+    if n < 0:
+        return 'Número igual ou menor a zero.'
+    # Ação
+    print(n)
+    # Caso recursivo
+    contagem_regressiva(n - 1)
+
+contagem_regressiva(10)
