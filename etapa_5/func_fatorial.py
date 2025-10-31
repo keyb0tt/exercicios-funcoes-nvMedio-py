11dc00e2f5d2d3eb1b95c268248bc552109b2e3a
Kaique <kaiqueboscoaraujo00@gmail.com>
2025-10-31 10:08:21 -0300

    .

--- etapa_5/func_fatorial.py
+++ etapa_5/func_fatorial.py
@@ -3,4 +3,11 @@ import os
 os.system('clear')
 
 def fatorial(n):
+    # Caso base (aonde termina)
+    if n == 0 or n == 1:
+        return 1
+    else:
+    # Caso recursivo (reutiliza função)
+        return n * fatorial(n - 1)
     
+print(fatorial(5))
